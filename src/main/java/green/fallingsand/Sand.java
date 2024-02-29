937b9ec185554081ad5075886e71adfb17c938b3
Yael Green <118456307+YCGreen@users.noreply.github.com>
2024-02-28 20:23:29 -0500

    Created sand class

---
+++ src/main/java/green/fallingsand/Sand.java
@@ -0,0 +1,40 @@
+package green.fallingsand;
+
+public class Sand {
+    private int[][] field = new int[3][3];
+
+    /**
+     * @return the value in field
+     */
+    public int get(int x, int y) {
+        return field[y][x];
+    }
+
+    /**
+     * Sets the value in field to be 1
+     */
+    public void put(int x, int y) {
+        field[y][x] = 1;
+    }
+
+    /**
+     * Moves all sand down one square
+     */
+    public void fall() {
+
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+
+        for (int y = 0; y < field.length; y++) {
+            for (int x = 0; x < field[y].length; x++) {
+                builder.append(field[y][x]);
+            }
+            builder.append("\n");
+        }
+
+        return builder.toString();
+    }
+}
