04542c7f89bd633cba6f8724ae6afa0744501e01
Yael Green <118456307+YCGreen@users.noreply.github.com>
2024-03-06 18:11:33 -0500

    checkstyles

--- src/main/java/green/fallingsand/Sand.java
+++ src/main/java/green/fallingsand/Sand.java
@@ -23,10 +23,10 @@ public class Sand {
      * Moves all sand down one square
      */
     public void fall() {
-        for(int y = 0; y < fieldY; y++) {
-            for(int x = 0; x < fieldX; x++) {
-                if(field[y][x] == 1 && y < fieldY - 1 && field[y + 1][x] == 0){
-                    field[y+1][x] = 1;
+        for (int y = 0; y < fieldY; y++)  {
+            for (int x = 0; x < fieldX; x++)  {
+                if (field[y][x] == 1 && y < fieldY - 1 && field[y + 1][x] == 0){
+                    field[y + 1][x] = 1;
                     field[y][x] = 0;
                 }
             }
