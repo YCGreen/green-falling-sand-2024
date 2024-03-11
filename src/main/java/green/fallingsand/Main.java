0419ab509eb51f669f3be330fc84e540ae20509e
Yael Green <118456307+YCGreen@users.noreply.github.com>
2024-03-11 12:53:59 -0400

    semantics, removed idea

--- src/main/java/green/fallingsand/Main.java
+++ src/main/java/green/fallingsand/Main.java
@@ -3,13 +3,15 @@ package green.fallingsand;
 import java.io.IOException;
 
 public class Main {
+    public static int ENTER = 10;
+
     public static void main(String[] args) {
         Sand sand = new Sand(50, 10);
         sand.randomSand(50);
         System.out.println(sand.toString());
         try {
-            while (System.in.read() == 10) {
-                onClick(sand);
+            while (System.in.read() == ENTER) {
+                onEnter(sand);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -18,7 +20,7 @@ public class Main {
 
     }
 
-    public static void onClick(Sand sand) {
+    public static void onEnter(Sand sand) {
         sand.fall();
         System.out.println(sand.toString());
     }
