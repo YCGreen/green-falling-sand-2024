12f9e9d51bce834fa3184ef5ca2ad1c05ee4de32
Yael Green <118456307+YCGreen@users.noreply.github.com>
2024-03-13 14:18:44 -0400

    changed enter key

--- src/main/java/green/fallingsand/Main.java
+++ src/main/java/green/fallingsand/Main.java
@@ -2,15 +2,15 @@ package green.fallingsand;
 
 import java.io.IOException;
 
-public class Main {
-    public static int ENTER = 10;
+import static java.awt.event.KeyEvent.VK_ENTER;
 
+public class Main {
     public static void main(String[] args) {
         Sand sand = new Sand(50, 10);
         sand.randomSand(50);
         System.out.println(sand.toString());
         try {
-            while (System.in.read() == ENTER) {
+            while (System.in.read() == VK_ENTER) {
                 onEnter(sand);
             }
         } catch (IOException e) {
