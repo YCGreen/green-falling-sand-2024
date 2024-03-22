967bf227d35e8626ccd865cc2f48a39f190090d4
Yael Green <118456307+YCGreen@users.noreply.github.com>
2024-03-21 22:25:37 -0400

    negligible

--- src/main/java/green/fallingsand/Main.java
+++ src/main/java/green/fallingsand/Main.java
@@ -3,20 +3,7 @@ package green.fallingsand;
 public class Main {
     public static void main(String[] args) {
         new SandFrame().setVisible(true);
-      /*  Sand sand = new Sand(4, 5);
-
-        String str = sand.toString();
-        System.out.println(str);
-        //     sand.resize(3, 8);
-        //    System.out.println(sand.toString());
-        Sand newSand = new Sand(5, 8);
-        newSand.load(str);
-        System.out.println(newSand.toString());
-
-        sand.put(2, 1);
-        sand.put(1, 0);
-        sand.put(2, 2);
-        System.out.println(sand.toString()); */
+     
     }
 
 }
